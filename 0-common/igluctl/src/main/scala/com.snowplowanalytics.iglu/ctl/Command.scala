d8183ea7204c81c72e06c355b49768108f5c7dea
Kevin Irwin <kevin@onespot.com>
2017-04-28 16:34:49 -0500

    Add ability to add a suffix to packages of generated classes. Handle fields with multiple types.

--- 0-common/igluctl/src/main/scala/com.snowplowanalytics.iglu/ctl/Command.scala
+++ 0-common/igluctl/src/main/scala/com.snowplowanalytics.iglu/ctl/Command.scala
@@ -63,7 +63,10 @@ case class Command(
   accessKeyId:     Option[String]  = None,
   secretAccessKey: Option[String]  = None,
   profile:         Option[String]  = None,
-  region:          Option[String]  = None
+  region:          Option[String]  = None,
+
+  // java
+  packageSuffix:   String          = ""
 
 ) {
   def toCommand: Option[Command.CtlCommand] = command match {
@@ -74,7 +77,7 @@ case class Command(
     case Some("static s3cp") =>
       Some(S3cpCommand(input.get, bucket.get, s3path, accessKeyId, secretAccessKey, profile, region))
     case Some("static java") =>
-      Some(GenerateJavaCommand(input.get, output.getOrElse(new File("."))))
+      Some(GenerateJavaCommand(input.get, output.getOrElse(new File(".")), packageSuffix))
     case Some("lint") =>
       Some(LintCommand(input.get, skipWarnings, linters))
     case _ =>
@@ -267,6 +270,11 @@ object Command {
             arg[File]("input") required()
               action { (x, c) => c.copy(input = Some(x))}
               text "Path to directory with JSON Schemas",
+
+            opt[String]("package-suffix")
+              action { (x, c) => c.copy(packageSuffix = x)}
+              valueName "<string>"
+              text "Suffix to apply to vendor name to construct package name of generated class for a schema",
 
             opt[File]("output")
               action { (x, c) => c.copy(output = Some(x)) }
